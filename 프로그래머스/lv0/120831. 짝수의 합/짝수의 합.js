c2bcda5ffe11d34b8d04ce2fd4cfd13b758b654c
YUJIN JI <131226548+zidoopal@users.noreply.github.com>
2023-07-30 04:39:18 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,8 +1,8 @@
 function solution(n) {
   // 정수 n 이하 모든 짝수 더하기
   let ans = 0;
-  for (let i = 2; i <= n; i+=2) {
-    if (i % 2 === 0) ans += i;
+  for (let i = 2; i <= n; i += 2) {
+    ans += i;
   }
   return ans;
 }

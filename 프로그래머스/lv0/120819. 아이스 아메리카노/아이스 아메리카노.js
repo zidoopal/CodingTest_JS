52adeb932da5d4df61251002067aad854b21ac35
YUJIN JI <131226548+zidoopal@users.noreply.github.com>
2023-07-31 12:30:12 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,10 +1,7 @@
 // 아메리카노 한 잔 5,500원 , 가진 돈 ==money 일 때, 최대로 마실 수 있는 아메리카노 잔 수와 남는 돈?
 
 function solution(money) {
-  // money / 5500 의 몫 == 아메리카노 잔 수, 나머지 == money-(몫[아메리카노 잔 수]*5500)
-  let ans = [];
   const coffee = Math.floor(money / 5500);
-  const change = money - coffee * 5500;
-  ans.push(coffee, change)
-  return ans;
+  const change = money % 5500
+  return [coffee, change]
 }

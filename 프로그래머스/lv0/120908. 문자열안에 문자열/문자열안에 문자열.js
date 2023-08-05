a1108e9ca604b1e678b24de09357f24a57f5fe2b
YUJIN JI <131226548+zidoopal@users.noreply.github.com>
2023-08-05 20:34:52 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,3 +1,20 @@
 function solution(str1, str2) {
-  return str1.includes(str2) ? 1 : 2;
+const len1 = str1.length;
+const len2 = str2.length;
+
+for (let i = 0; i <= len1 - len2; i++) {
+let found = true;
+
+for (let j = 0; j < len2; j++) {
+if (str1[i + j] !== str2[j]) {
+found = false;
+break;
+}
+}
+
+if (found) {
+return 1;
+}
+}
+return 2;
 }

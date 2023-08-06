969bb39fcc40f384c813c3484985918e02cd9fb4
YUJIN JI <131226548+zidoopal@users.noreply.github.com>
2023-08-06 10:35:31 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,3 +1,9 @@
 function solution(num_list) {
-  return num_list.reverse();
+  // 메서드 없이 풀어보기
+  ans = [];
+  for (i = 1; i <= num_list.length; i++) {
+    // num_list[num_list.length -1]씩 ans에 넣어서 return해주기
+    ans.push(num_list[num_list.length - i]);
+  }
+  return ans;
 }

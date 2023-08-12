948e6d3a6640045b544d73af151c57bb7244dc91
YUJIN JI <131226548+zidoopal@users.noreply.github.com>
2023-08-12 15:47:37 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,8 +1,6 @@
 function solution(my_string) {
   let ans = 0;
-  for (i = 0; i < my_string.length; i++) {
-    if (isNaN(my_string[i])) {
-    } else ans += parseInt(my_string[i]);
-  }
+  // 바로 Number, parseInt 사용 못함, 문자로 시작하면 둘다 NaN 반환해버림
+  my_string.split('').filter((item) => +item).forEach((num)=> ans+=(+num)) // 숫자만 배열로 반환 후 다 더해
   return ans;
 }

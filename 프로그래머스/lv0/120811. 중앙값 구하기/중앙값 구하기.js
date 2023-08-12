0b1b6db72242e9cc3ccd84a92589f7b5be6ba786
YUJIN JI <131226548+zidoopal@users.noreply.github.com>
2023-08-12 16:25:12 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,5 +1,6 @@
 function solution(array) {
-  // (1) array.sort((a, b) => a - b); // 오름차순 정렬
-  // (2) [Math.floor(array.length / 2)] // array 길이 / 2  의 몫== 중앙값
-  return array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
+  let num = Math.floor(array.length / 2)
+  // array배열을 오름차순으로 정렬
+  array.sort((a,b)=>a-b)
+  return array[num]
 }

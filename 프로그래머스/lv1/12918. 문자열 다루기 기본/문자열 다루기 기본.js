854b7d3cce3bf7b6d101d9d50fc175d69a0e1816
YUJIN JI <zidoopal2@gmail.com>
2023-08-23 12:13:16 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,5 +1,8 @@
+
 function solution(s) {
-  // 정규식
+  // s.length가 4 or 6 && typeof(s) == number 확인해주는 함수 만들기 >> 정규식 사용하기
   const regex = /^\d{4}$|^\d{6}$/;
+  // (정규표현식).test("문자열")
+  // "문자열"이 "정규표현식"과 매칭되면 true, 아니면 false반환
   return regex.test(s);
 }

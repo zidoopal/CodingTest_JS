7954d03affe4d772a1910b6b898a4d350bbf9bf0
YUJIN JI <zidoopal2@gmail.com>
2023-08-23 14:09:15 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.07 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,11 @@
+
+function solution(arr1, arr2) {
+  var ans = [[]];
+  for (let i = 0; i < arr1.length; i++) {
+    ans[i] = [];
+    for (let j = 0; j < arr1[i].length; j++) {
+      ans[i].push(arr1[i][j] + arr2[i][j]);
+    }
+  }
+  return ans;
+}

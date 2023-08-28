914e103b78397603ce533491a8717da4133002dd
YUJIN JI <zidoopal2@gmail.com>
2023-08-28 09:36:35 +0900

    [level 1] Title: 직사각형 별찍기, Time: 51.67 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/README.md
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 직사각형 별찍기 - 12969 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969) 
 
 ### 성능 요약
 
-메모리: 32.3 MB, 시간: 69.96 ms
+메모리: 32.3 MB, 시간: 51.67 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,9 +2,8 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    const star = '*'.repeat(a)
-    for(let i =0; i < b; i++){
-        console.log(star)
-    }
-
+const star = '*'.repeat(a);
+for(let i=0; i<b; i++) {
+    console.log(star)
+}
 });

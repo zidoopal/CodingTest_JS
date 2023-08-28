477694d675a4f33916d91b96c95fd840624f9ca3
YUJIN JI <zidoopal2@gmail.com>
2023-08-28 09:55:33 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,7 +1,7 @@
-function solution(a, b) {
-    var gcd = function(b, a) {
-      var r = (b % a);
-      return (r) ? gcd(a, r) : a;
-    }
-    return [gcd(b, a), ((b * a) / gcd(b, a))];
+function solution(n, m) {
+  const gcd = (m, n) => {
+    const r = m % n;
+    return r ? gcd(n, r) : n;
+  };
+  return [gcd(m, n), (m * n) / gcd(m, n)];
 }

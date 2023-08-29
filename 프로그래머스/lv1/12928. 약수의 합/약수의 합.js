a41b1587dde1d8999a08c2b12cdfdd2cd1ea8ebe
YUJIN JI <zidoopal2@gmail.com>
2023-08-29 17:03:57 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,11 +1,10 @@
 function solution(n) {
-  // 약수를 모두  더한 값을 넣어줄(누적 할) sum 변수 선언 및 초기화   
-  let sum = 0
-  // for반복문 사용, n의 제곱근의 범위 만큼 반복해주기
-  for (let i = 1; i <= n; i++) {
-    // n을 i로 나눴을 때 나머지가 0일 경우 sum에 i 더해주기
-    if (n % i === 0) sum += i;
+  let sum = 0;
+  // n을 나눴을 때 나머지가 0이 되는 수 모두 배열에 담고 이거 다 더한 수 return  하기
+  for (let i = 0; i <= n; i++) {
+    if (n % i === 0) {
+      sum += i;
+    }
   }
-    // sum 리턴
-    return sum
+  return sum;
 }

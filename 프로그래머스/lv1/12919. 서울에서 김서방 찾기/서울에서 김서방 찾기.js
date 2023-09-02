d99b7e73371be444ecde72a5df7289d05a59fc13
YUJIN JI <zidoopal2@gmail.com>
2023-09-02 09:22:19 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,8 +1,3 @@
 function solution(seoul) {
-  //indexOf() 사용하기
-  const indexOfKim = seoul.indexOf('Kim');
-
-  return `김서방은 ${indexOfKim}에 있다`;
+  return `김서방은 ${seoul.indexOf('Kim')}에 있다`;
 }
-
-console.log(solution(['Jane', 'Kim']));

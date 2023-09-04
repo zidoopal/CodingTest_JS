fde478fb5124cd136a3f953b30976c2a50857c51
YUJIN JI <zidoopal2@gmail.com>
2023-09-04 09:15:31 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -2,7 +2,7 @@ function solution(absolutes, signs) {
   let sum = 0;
 
   for (let i = 0; i < absolutes.length; i++) {
-    signs[i] === true ? (sum += absolutes[i]) : (sum += absolutes[i] * -1);
+    signs[i] === true ? sum += absolutes[i] :sum -= absolutes[i]
   }
 
   return sum;

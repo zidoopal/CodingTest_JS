6f708eeb4bd8a7969a106a405cbc9ff7c5a72b97
YUJIN JI <zidoopal2@gmail.com>
2023-09-04 09:56:42 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,10 @@
 function solution(numbers) {
-  let ans = 0;
-  // numbers 0~9까지 없어? 글ㄹ엄 찾아! 없는 거 더해! 릿헌 해!
-  for (let i = 0; i <= 9; i++) {
-    if (!numbers.includes(i)) ans += i;
-  }
+  let sum = 0;
 
-  return ans;
+  for (let i = 0; i <= 9; i++)
+    if (!numbers.includes(i)) {
+      sum += i;
+    }
+
+  return sum;
 }

982616c0890d3fe1ab5c10e7a7de475c2515e989
YUJIN JI <zidoopal2@gmail.com>
2023-09-04 09:33:40 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,3 @@
 function solution(phone_number) {
-  //phone_number 뒷 자리 4 제외하고 다 *로 바꿔바꿔 // 정규 표현식 사용
   return phone_number.replace(/\d(?=\d{4})/g, '*');
 }
-
-console.log(solution('01033334444'));

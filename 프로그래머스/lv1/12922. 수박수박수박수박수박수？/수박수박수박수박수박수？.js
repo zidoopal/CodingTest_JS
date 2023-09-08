8dced11819d546d80057b200d480a48f265bab95
YUJIN JI <zidoopal2@gmail.com>
2023-09-08 09:13:28 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,10 +1,10 @@
-// 수박수박수박
 function solution(n) {
-  var answer = '';
-  var w = '수박';
-  // answer에 수박 삽입
-  for (let i = 0; i < Math.floor(n / 2); i++) answer += w;
-  // 만약 n이 홀수면 마지막 인자 제외한 값을 answer에 삽입
-  if (n % 2 == 1) answer += '수';
-  return answer;
+  let ans = [];
+
+  for (let i = 1; i <= n; i++) {
+    i % 2 !== 0 ? ans.push('수') : ans.push('박');
+  }
+  // 홀일때 '수'
+  // 짝일때 '박'
+  return ans.join('');
 }

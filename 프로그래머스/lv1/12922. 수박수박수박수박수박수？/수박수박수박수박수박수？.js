d0411700841857287128dd35bfd62f013bdfef73
YUJIN JI <zidoopal2@gmail.com>
2023-09-08 09:15:09 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,10 +1,10 @@
 function solution(n) {
-  let ans = [];
+  let ans = '';
 
   for (let i = 1; i <= n; i++) {
-    i % 2 !== 0 ? ans.push('수') : ans.push('박');
+    i % 2 !== 0 ? (ans += '수') : (ans += '박');
   }
   // 홀일때 '수'
   // 짝일때 '박'
-  return ans.join('');
+  return ans;
 }
